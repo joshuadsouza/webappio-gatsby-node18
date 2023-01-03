8e0765650e042a5bf07c8cbaa71c60e4d647400c
Joshua D'Souza <joshua.dsouza48@gmail.com>
2023-01-03 12:27:38 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `webapp.io Gatsby Node 18`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
